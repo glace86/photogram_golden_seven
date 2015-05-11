a6e1ae7c99616ac4e294e94ed27095304eb9719a
George Lace <glace2015@kellogg.northwestern.edu>
2015-05-11 18:04:30 -0500

    HW6_George

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,43 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @number = params[:id]
+    @list_of_photos = Photo.all
+    @photoreq = @list_of_photos.find(@number)
+  end
+
+  def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    i = params[:id]
+    p = Photo.find(i)
+    p.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    i = params[:id]
+    @photonw = Photo.find(i)
+  end
+
+  def update_row
+    i = params[:id]
+    p = Photo.find(i)
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+  end
 
 end
